e96351ffe8dcfded167118c9562585a6411ab4f8
Joseph Bouqdib <joseph@bouqdib.com>
2024-11-05 11:58:49 +0000

    Add a hover color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
         screen: "#2a2a36",
         highlight: "#e74043",
         tint: "#ebaa39",
+        hover: "#f1643a",
         icon: "#9BA1A6",
         text: "#ECEDEE",
         subText: "#9BA1A6",
