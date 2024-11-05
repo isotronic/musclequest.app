7a2f24e39aa34ed8f96901d8915c50ac8157a049
Joseph Bouqdib <joseph@bouqdib.com>
2024-11-05 08:35:02 +0000

    Add gtag

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,14 +2,32 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `musclequest.app`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `MuscleQuest`,
+    siteUrl: `https://musclequest.app`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-google-gtag"]
+  plugins: [
+    "gatsby-plugin-postcss",
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-463YZ9YPK0", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          //optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+      },
+    },
+  ],
 };
 
 export default config;
