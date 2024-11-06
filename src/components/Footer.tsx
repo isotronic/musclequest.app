d38832c66d15ff5309b034d172c63d346f3a3ad4
Joseph Bouqdib <joseph@bouqdib.com>
2024-11-06 13:41:59 +0000

    Link to my portfolio

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,12 @@ import React from "react";
 const Footer: React.FC = () => {
   return (
     <footer className="py-6 text-center">
-      <p>&copy; {new Date().getFullYear()} by Joseph Bouqdib</p>
+      <p>
+        &copy; {new Date().getFullYear()} by{" "}
+        <a className="transition hover:text-tint" href="https://joseph.bouqdib.com" target="_blank">
+          Joseph Bouqdib
+        </a>
+      </p>
     </footer>
   );
 };
