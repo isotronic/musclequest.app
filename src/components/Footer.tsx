3449521af994e3b687ca30a3034f12f5d4119821
Joseph Bouqdib <joseph@bouqdib.com>
2024-11-06 09:26:16 +0000

    Remove social icons from footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,29 +1,8 @@
 import React from "react";
-import { FaGithub, FaLinkedin } from "react-icons/fa6";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-slate-900 py-6 text-center text-slate-400">
-      <div className="mb-4 flex justify-center space-x-6 md:hidden">
-        <a
-          href="https://github.com/isotronic"
-          target="_blank"
-          rel="noreferrer"
-          aria-label="GitHub"
-          className="transition hover:text-green-500"
-        >
-          <FaGithub size={24} />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/jbouqdib/"
-          target="_blank"
-          rel="noreferrer"
-          aria-label="LinkedIn"
-          className="transition hover:text-green-500"
-        >
-          <FaLinkedin size={24} />
-        </a>
-      </div>
+    <footer className="py-6 text-center">
       <p>&copy; {new Date().getFullYear()} by Joseph Bouqdib</p>
     </footer>
   );
