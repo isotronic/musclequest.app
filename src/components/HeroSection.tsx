25d6dafa1e8b727b4823d4afbb331fe0c59c716e
Joseph Bouqdib <joseph@bouqdib.com>
2024-11-06 13:47:59 +0000

    Adjust padding and image size in hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,7 +5,7 @@ import HomeScreenshot from "../images/home-screen.jpg";
 
 const HeroSection: React.FC = () => {
   return (
-    <section className="flex min-h-screen max-w-7xl flex-col items-center justify-center py-20 text-center md:m-auto md:flex-row">
+    <section className="flex min-h-screen max-w-7xl flex-col items-center justify-center py-28 text-center md:m-auto md:flex-row">
       <div className="mx-auto mb-12 flex max-w-lg flex-col items-center md:mb-0 md:mr-24">
         <h1 className="mb-8 text-4xl font-bold md:text-5xl">
           Transform Your Workouts with <span className="italic">MuscleQuest</span>
@@ -31,7 +31,7 @@ const HeroSection: React.FC = () => {
           src={HomeScreenshot}
           alt="App Screenshot"
           className="rounded-lg shadow-md"
-          style={{ maxHeight: "700px" }}
+          style={{ maxHeight: "675px" }}
         />
       </div>
     </section>
