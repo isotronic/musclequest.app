746ecf68ee06d73113d122ff88fabaa92859f0cd
Joseph Bouqdib <joseph@bouqdib.com>
2025-02-05 12:33:44 +0000

    Change wording in contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,8 +6,8 @@ const Contact: React.FC = () => {
     <section id="contact" className="flex flex-col items-center bg-screen px-6 py-28">
       <h2 className="mb-8 text-center text-3xl font-bold">Connect With MuscleQuest</h2>
       <p className="mb-8 max-w-md text-center">
-        Follow us on social media or reach out via email for support, feedback, or questions. We’d love to hear from
-        you!
+        Follow MuscleQuest on social media or reach out via email or DM for support, feedback, or questions. I’d love to
+        hear from you!
       </p>
 
       <div className="flex space-x-12">
